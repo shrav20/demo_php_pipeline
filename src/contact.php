1dcfa4764671c6ef97af9945162d3d12b1a66357
shrav20 <91880988+shrav20@users.noreply.github.com>
2024-05-01 14:50:55 +0530

    Add files via upload

---
+++ src/contact.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>EVENT</title>
+        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+        <head>
+    <body>
+        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
+        <div class = "content"><!--body content holder-->
+            <div class = "container">
+                <div class = "col-md-12"><!--body content title holder with 12 grid columns-->
+                    <h1 style="color:#003300 ; font-size:38px ;"><strong>Contact Us</strong></h1><!--body content title-->
+                </div>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+            
+            <div class="container">
+                <div class="col-md-6 contacts">
+                    <h1 style="color:#000080 ; font-size:42px ; font-style:bold "><span class="glyphicon glyphicon-user"></span>DBATU,Lonere</h1>
+                    <p><P>The Registrar
+ Dr. Babasaheb Ambedkar Technological University,
+Lonere, Tal- Mangaon, Dist – Raigad. Maharashtra (India). 402103
+</P>
+                        <span class="glyphicon glyphicon-envelope"></span> 
+ Email: registrar@dbatu.ac.in<br>
+    
+                        <span class="glyphicon glyphicon-phone"></span> Mobile: 7777777777
+                    </p>
+                </div>
+                <div class="col-md-6 contacts">
+                    
+            </div>
+			
+            
+        </div><!--body content div-->
+        <?php require 'utils/footer.php'; ?><!--footer content. file found in utils folder-->
+    </body>
+</html>
